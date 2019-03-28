61d20cbefbedc9573e30af5d70a35093dac8513c
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-03-28 21:41:39 +1100

    passed in the user's name to the dashboard get request to the user can have a personalized welcome upon login

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,10 @@ const { ensureAuthenticated } = require('../config/auth');
 router.get('/', (req, res) => res.render('welcome'));
 
 //Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard'));
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+    res.render('dashboard', {
+        name: req.user.name
+    }));
 
 
 module.exports = router;
