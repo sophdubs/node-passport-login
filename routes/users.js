6034b757f4696e9c8532459aa51db3b4431de646
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-03-28 21:15:08 +1100

    added some passport authetication code in users.js for router.post login requests. No functionality yet, just reloads the login page when given email that is not registered

--- routes/users.js
+++ routes/users.js
@@ -90,6 +90,16 @@ router.post('/register', (req, res) => {
 
 });
 
+//Login handle
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
+});
+
 
 
 module.exports = router;
